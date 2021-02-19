6d677e05de57b87e214fb8f518eb0a2f6197b7b6
Brandon Perard <brandon.perard@gmail>
2021-02-18 18:22:00 -0800

    update files

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
-const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.use(cors());
+
+app.get('/yo', (req, res) => {
+    res.send('yo')
 })
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const { app } = require('./app.js');
 const port = process.env.PORT || 3000
 
-// we will define our port for local development
-const port = 3000;
 
 // this is the code to 'launch/spin-up' our server.
 app.listen(port, () => {
